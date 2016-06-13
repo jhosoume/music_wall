c40185cbf849f3acee9f873ae98f1fb8cda1220e
Juliana Hosoume <ju.hosoume@gmail.com>
2016-06-13 14:08:28 -0700

    List songs

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,26 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'/songs/index'
+end 
+
+get '/songs/new' do
+  erb :'/songs/new/index'
+end
+
+post '/songs' do
+  @song = @Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else 
+    erb :'/songs/new'
+  end
+end
+
